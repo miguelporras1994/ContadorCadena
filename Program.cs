8aa1a035685ba3ab718b86f0848f7dddaf6adb7d
Miguel Porras <porraangel@gmail.com>
2021-11-03 22:44:47 -0500

    Numero Mayor

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CadenaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] words = { 1, 50, 80, 12, 8, 5 };
+
+            int count = words.Length;
+            int num  = 0, contador = 0;
+            
+
+            foreach (var i in words) 
+            {
+                contador = contador + 1;
+
+                if (num <= i)
+                {
+                    num = i;
+                   
+                }
+                if (count == contador)
+                {
+                    Console.WriteLine("numero mas grande de la lista es : " + num);
+
+
+                }
+
+            }
+        }
+    }
+}
